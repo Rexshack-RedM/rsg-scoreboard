1a27da9af4a12765f7ee5362385c54acca273ee4
RexShack <rexshackuk@gmail.com>
2022-12-24 07:06:46 +0000

    Initial commit

---
+++ config.lua
@@ -0,0 +1,21 @@
+Config = Config or {}
+
+-- Open scoreboard key
+Config.OpenKey = 0x05CA7C52
+
+-- Max Server Players
+Config.MaxPlayers = GetConvarInt('sv_maxclients', 64) -- It returnes 64 if it cant find the Convar Int
+
+-- Minimum Police for Actions
+Config.IllegalActions = {
+    ["storerobbery"] = {minimum = 2, busy = false},
+    ["trainrobbery"] = {minimum = 3, busy = false},
+    ["bankrobbery"] = {minimum = 3, busy = false},
+    ["wagonrobbery"] = {minimum = 5, busy = false}
+}
+
+-- Current Cops Online
+Config.CurrentCops = 0
+
+-- Current Ambulance / Doctors Online
+Config.CurrentAmbulance = 0
